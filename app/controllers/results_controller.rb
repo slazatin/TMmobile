78ee2c029b8acc016fd33690a6bfa2e32c3b92ba
Steven Lazatin <steven@Steven-Lazatins-MacBook-Pro.local>
2014-07-21 15:07:21 -0700

    styling changes on mural

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -11,7 +11,7 @@ class ResultsController < ApplicationController
   def show
     @song = Song.find(params[:id])
     @artistimg = Song.google_image("#{@song.artist} + #{@song.year}" )
-    @cityimg = Song.google_image("#{@song.place} + #{@song.year}" )
+    @cityimg = Song.google_image("#{@song.place} + #{@song.year} + 'photos'" )
     @fashionimg = Song.google_image("'getty images' + #{@song.year} + 'fashion'" )
     @artimg = Song.google_image("'getty images' + #{@song.year} + 'art'" )
     @filmimg = Song.google_image("'getty images' + #{@song.year} + 'film'" )
